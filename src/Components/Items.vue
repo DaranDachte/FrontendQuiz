307d9d476c4d41aff0831bb94ccf52341018acc7
Daran Dachte <darandachte@gmail.com>
2023-12-25 21:12:50 +0100

    home layout created

---
+++ src/Components/Items.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import type { ItemProps } from "./Item.vue";
+
+import HtmlSvg from "../assets/img/Html.svg";
+import CssSvg from "../assets/img/Css.svg";
+import JSSvg from "../assets/img//JS.svg";
+import AccessibilitySvg from "../assets/img/Accessibility.svg";
+
+import Item from "./Item.vue";
+const list: ItemProps[] = [
+  {
+    text: "HTML",
+    altText: "HTMLSvg",
+    svgPath: HtmlSvg,
+  },
+  {
+    text: "CSS",
+    altText: "CSS",
+    svgPath: CssSvg,
+  },
+  {
+    text: "JavaScript",
+    altText: "JS",
+    svgPath: JSSvg,
+  },
+  {
+    text: "Accessibility",
+    altText: "AccessibilitySvg",
+    svgPath: AccessibilitySvg,
+  },
+];
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <Item
+      v-for="element in list"
+      :svg-path="element.svgPath"
+      :alt-text="element.altText"
+      :text="element.text"
+      :key="element.text"
+    />
+  </div>
+</template>
